671909e1fbb329080fca0cce6e0e4c50e28ff348
22BCS16886@cuchd.in <22BCS16886@cuchd.in>
2026-02-06 18:45:01 +0530

    asd

--- perioxiaweb/.next/dev/static/chunks/perioxiaweb_app_page_tsx_2faf5caa._.js
+++ perioxiaweb/.next/dev/static/chunks/perioxiaweb_app_page_tsx_2faf5caa._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/perioxiaweb_app_components_28148adc._.js",
-  "static/chunks/1b5a7_fd502e9f._.js"
+  "static/chunks/1b5a7_7222cd3b._.js",
+  "static/chunks/perioxiaweb_app_components_92cd3fb5._.js"
 ],
     source: "dynamic"
 });
